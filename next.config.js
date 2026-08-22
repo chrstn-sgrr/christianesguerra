9585fcbb142b20da514827dce92e35c4b8ada424
chrstn-sgrr <christian9esguerra@gmail.com>
2026-08-22 21:32:05 +0800

    chore: config static export and basePath for GitHub Pages

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/christianesguerra",
+  images: { unoptimized: true },
+  trailingSlash: true,
+};
+
+module.exports = nextConfig;
